8221b766d4c237409a4f80957e336a4cbd2f43fb
navalt7 <navalthakur70@outlook.com>
2024-07-07 16:07:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
+
+// Use environment variable for MongoDB URI or fallback to default
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://navalthakur70:RfDyFM6oydhjD4M2@myinstagramdb-cluster.ueymcds.mongodb.net/?tls=true';
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://localhost:27017/myInstagramDB', { useUnifiedTopology: true, useNewUrlParser: true })
+// Connect to MongoDB Atlas
+mongoose.connect(mongoURI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.error(error));
+  .catch(error => console.error('Error connecting to MongoDB:', error));
 
 const userSchema = new mongoose.Schema({
   instagramAccount: { type: String, unique: true },
@@ -31,25 +35,25 @@ app.post('/register', async (req, res) => {
   console.log(`Received registration request for: ${instagramAccount}`);
   
   try {
-      const existingUser = await User.findOne({ instagramAccount });
+    const existingUser = await User.findOne({ instagramAccount });
 
-      if (existingUser) {
-          console.log('User already exists');
-          return res.status(400).json({ message: 'User already exists', success: false });
-      }
+    if (existingUser) {
+      console.log('User already exists');
+      return res.status(400).json({ message: 'User already exists', success: false });
+    }
 
-      const newUser = new User({ instagramAccount, password });
-      await newUser.save();
+    const newUser = new User({ instagramAccount, password });
+    await newUser.save();
 
-      console.log('User registered successfully');
-      res.json({
-          message: 'User registered successfully',
-          success: true,
-          redirectTo: '/login.html'
-      });
+    console.log('User registered successfully');
+    res.json({
+      message: 'User registered successfully',
+      success: true,
+      redirectTo: '/login.html'
+    });
   } catch (error) {
-      console.error('Error during registration:', error);
-      res.status(500).json({ message: 'Internal Server Error', success: false });
+    console.error('Error during registration:', error);
+    res.status(500).json({ message: 'Internal Server Error', success: false });
   }
 });
 
@@ -66,7 +70,7 @@ app.post('/login', async (req, res) => {
     res.cookie('session', 'authenticated');
     res.json({ success: true, message: 'Logged in successfully' });
   } catch (error) {
-    console.error(error);
+    console.error('Error during login:', error);
     res.status(500).json({ message: 'Internal Server Error', success: false });
   }
 });
